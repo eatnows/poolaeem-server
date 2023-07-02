b4c2177c213bb0fed8b5f225f90ccc6c53fabab8
eatnows <gagyasindev@gmail.com>
2023-07-02 23:16:17 +0900

    feat: #13 - JPA Auditing 설정 추가

---
+++ poolaeem-api/src/main/java/com/poolaeem/poolaeem/config/jpa/UserIdAuditorAware.java
@@ -0,0 +1,26 @@
+package com.poolaeem.poolaeem.config.jpa;
+
+import com.poolaeem.poolaeem.common.exception.auth.UnAuthorizationException;
+import com.poolaeem.poolaeem.security.jwt.token.CustomUserDetail;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class UserIdAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = Optional.ofNullable(
+                        SecurityContextHolder.getContext().getAuthentication())
+                .orElseThrow(UnAuthorizationException::new);
+
+        if (!authentication.isAuthenticated()
+                || authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList().contains("ROLE_ANONYMOUS")) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(((CustomUserDetail) authentication.getPrincipal()).getUser().getId());
+    }
+}
