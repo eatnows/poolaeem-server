8c80c16d16350296a19f3bdfb1f5634cc6e4586e
eatnows <gagyasindev@gmail.com>
2023-07-05 19:44:43 +0900

    refactor: #22 - 문제집의 문제 수 칼럼 이름 변경
    - 문제집의 문항 개수를 담고 있는 칼럼의 이름을 변경
      - `questionCount` -> `problemCount`

--- poolaeem-api/src/main/resources/db/migration/V6__add_workbook_table.sql
+++ poolaeem-api/src/main/resources/db/migration/V6__add_workbook_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE workbook (
     `user_id` varchar(32) not null,
     `name` varchar(200) not null,
     `description` varchar(500) not null,
-    `question_count` int(11) not null,
+    `problem_count` int(11) not null,
     `solved_count` int(11) not null,
     `theme` varchar(15) not null,
     `workbook_order` int(11) default 1 not null,
--- poolaeem-api/src/test/resources/sql/question/workbook.sql
+++ poolaeem-api/src/test/resources/sql/question/workbook.sql
@@ -1,6 +1,6 @@
 delete from `workbook`;
 
-insert into `workbook` (id, user_id, name, description, question_count, solved_count, theme, workbook_order, is_deleted,
+insert into `workbook` (id, user_id, name, description, problem_count, solved_count, theme, workbook_order, is_deleted,
                         created_by, updated_by, created_at, updated_at)
 values ('workbook-1', 'user-1', '고등영어1', '고등학교에서 사용하는 영단어 문제입니다.', 3, 2, 'PINK', 1, false, 'user-1', 'user-1',
         '2023-07-04 14:36:23.599', '2023-07-04 14:36:23.599'),
