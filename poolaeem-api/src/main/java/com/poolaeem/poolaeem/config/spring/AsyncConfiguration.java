cebe9e4b5ec74762a3ac5921fbe7da8b67390518
eatnows <gagyasindev@gmail.com>
2023-07-23 00:24:33 +0900

    feat: #30 - Async Thread 에 Authentication 전파
    - @Async 로 비동기 호출 시 하위 새 Thread에서 SecurityContext의 Authentication을 사용할 수 있도록 ThreadPoolTaskExecutor 빈 생성을 변경

--- poolaeem-api/src/main/java/com/poolaeem/poolaeem/config/spring/AsyncConfiguration.java
+++ poolaeem-api/src/main/java/com/poolaeem/poolaeem/config/spring/AsyncConfiguration.java
@@ -2,21 +2,22 @@ package com.poolaeem.poolaeem.config.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 
 @EnableAsync
 @Configuration
 public class AsyncConfiguration {
-    @Bean(name = "asyncThreadPoolTaskExecutor")
-    public Executor asyncThreadPoolTaskExecutor() {
+    @Bean("asyncThreadPoolTaskExecutor")
+    public TaskExecutor asyncThreadPoolTaskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(5);
         taskExecutor.setMaxPoolSize(20);
         taskExecutor.setQueueCapacity(30);
         taskExecutor.setThreadNamePrefix("Async-");
-        return taskExecutor;
+        taskExecutor.initialize();
+        return new DelegatingSecurityContextAsyncTaskExecutor(taskExecutor);
     }
 }
